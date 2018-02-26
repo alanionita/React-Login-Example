e75745822a7aa68b6851dbd19e2a9cf0a78c7039
alanionita <a.g.ionita@gmail.com>
2018-02-26 16:34:51 +0000

    add extra redux actions for validation

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -24,7 +24,7 @@ export function findApplicationByShortcodeFailed (err) {
 export function findApplicationByShortcode (shortcode) {
     return function (dispatch) {
         dispatch(findApplicationByShortcodeRequest());
-        return axios.get(`${process.env.REACT_APP_API_URL_APPLICATIONS}/applications/${shortcode}`)
+        return axios.get(`${process.env.REACT_APP_API_URL_APPLICATIONS}/applications/${shortcode}/validate`)
             .then(res => {
                 dispatch(findApplicationByShortcodeSuccess(res.data));
             })
@@ -34,3 +34,35 @@ export function findApplicationByShortcode (shortcode) {
     };
 }
 
+export function validateSignInDetailsRequest () {
+    return {
+        type: types.VALIDATE_SIGN_IN_DETAILS_REQUEST
+    };
+}
+
+export function validateSignInDetailsSuccess (validation) {
+    return {
+        type: types.VALIDATE_SIGN_IN_DETAILS_SUCCESS,
+        payload: validation
+    };
+}
+
+export function validateSignInDetailsFailed (err) {
+    return {
+        type: types.VALIDATE_SIGN_IN_DETAILS_FAILED,
+        payload: err
+    };
+}
+
+export function validateSignInDetails (shortcode, userInput) {
+    return function (dispatch) {
+        dispatch(validateSignInDetailsRequest());
+        return axios.post(`${process.env.REACT_APP_API_URL_APPLICATIONS}/documents/${shortcode}/validate`, userInput)
+            .then(res => {
+                dispatch(validateSignInDetailsSuccess(res.data));
+            })
+            .catch(err => {
+                dispatch(validateSignInDetailsFailed(err));
+            });
+    };
+}
--- src/actions/actions.spec.js
+++ src/actions/actions.spec.js
@@ -79,3 +79,78 @@ describe('APPLICATION ACTIONS', () => {
     });
   });
 });
+
+describe('SIGN IN ACTIONS', () => {
+  describe('#validateSignInDetailsRequest', () => {
+    test('returns \'VALIDATE SIGN IN DETAILS REQUEST\'', () => {
+      expect(actions.validateSignInDetailsRequest()).toEqual({
+        type: 'VALIDATE SIGN IN DETAILS REQUEST'
+      });
+    });
+  });
+  describe('#validateSignInDetailsSuccess', () => {
+    test('returns \'VALIDATE SIGN IN DETAILS SUCCESS\' and payload', () => {
+      const input = {
+        detailsValidated: true
+      };
+      expect(actions.validateSignInDetailsSuccess(input)).toEqual({
+        type: 'VALIDATE SIGN IN DETAILS SUCCESS',
+        payload: input
+      });
+    });
+  });
+  describe('#validateSignInDetailsFailed ', () => {
+    test('returns \'VALIDATE SIGN IN DETAILS FAILED\' and payload', () => {
+      const err = {
+        error: "Wrong document type and document number combination"
+      };
+      expect(actions.validateSignInDetailsFailed(err)).toEqual({
+        type: 'VALIDATE SIGN IN DETAILS FAILED',
+        payload: err
+      });
+    });
+  });
+  describe('#validateSignInDetails ASYNC', () => {
+    beforeEach(function () {
+      moxios.install();
+    });
+
+    afterEach(function () {
+      moxios.uninstall();
+    });
+
+    test('returns correct series of actions and payload if succesful', () => {
+      const shortcode = 'cats-like-mice';
+      const userInput = {
+        docType: "Passport",
+        docNumber: "1099099887"
+      }
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: {
+            detailsValidated: true
+          }
+        });
+      });
+
+      const store = mockStore({
+        application: null
+      });
+
+      const expectedActions = [
+        { type: types.VALIDATE_SIGN_IN_DETAILS_REQUEST },
+        {
+          type: types.VALIDATE_SIGN_IN_DETAILS_SUCCESS,
+          payload: {
+            detailsValidated: true
+          }
+        }
+      ];
+      return store.dispatch(actions.validateSignInDetails(shortcode, userInput)).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
+  });
+});
